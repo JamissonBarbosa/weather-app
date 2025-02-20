de456a1b0bab4bb8567dab30f2ad06b1658b3994
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2025-02-20 15:36:56 -0300

    feat: create router

---
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { WeatherData } from '../../models/WeatherData';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: ['./weather-card.component.scss']
+})
+export class WeatherCardComponent {
+  @Input() weatherCardInput!: WeatherData;
+
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+}
--- src/app/modules/weather/weather-home/weather-home.component.ts
+++ src/app/modules/weather/weather-home/weather-home.component.ts
@@ -1,32 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WeatherService } from '../services/weather.service';
+import { WeatherData } from '../models/WeatherData';
+import { Subject, takeUntil } from 'rxjs';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: ['./weather-home.component.scss']
 })
-export class WeatherHomeComponent implements OnInit {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+
+  initialCity = 'são Paulo';
+  weatherData!: WeatherData;
+  searchIcon = faMagnifyingGlass;
 
   constructor(private weatherService: WeatherService) { }
 
-  initialCity = 'London';
+
 
   ngOnInit(): void {
     this.listCities(this.initialCity);
   }
 
   listCities(cityName: string): void {
-    this.weatherService.getWeatherData(cityName).subscribe({
-      next: (response) => {
-        console.log(response);
-      },
-      error: (error) => {
-        console.error(error);
-      }
-    })
+    this.weatherService.getWeatherData(cityName)
+      .pipe(
+        takeUntil(this.destroy$)
+      )
+      .subscribe({
+        next: (response) => {
+          response && (this.weatherData = response);
+          console.log(this.weatherData);
+        },
+        error: (error) => {
+          console.error(error);
+        }
+      })
   }
 
+  searchCity(cityName: string): void {
+    cityName = this.initialCity;
+    this.listCities(cityName);
+    this.initialCity = '';
+  }
+
+
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
 
 
